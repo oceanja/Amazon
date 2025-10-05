b41cfd26297c12bacc23ac6e8971ff123c2dd349
Ocean <144541279+oceanja@users.noreply.github.com>
2025-10-06 04:10:30 +0530

    Update 0378-kth-smallest-element-in-a-sorted-matrix.cpp

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -32,3 +32,59 @@ public:
         
     }
 };
+
+
+// ************************ OPTIMIZED********************************
+class Solution {
+public:
+
+int count(int mid,vector<vector<int>>& matrix){
+
+    int n=matrix.size();
+    int cnt=0;
+
+    int row=n-1;
+    int col=0;
+
+    while(row>=0 && col<n){
+        if(matrix[row][col]<=mid){
+            cnt+=(row+1);
+            col++;
+        }
+        else{
+            row--;
+        }
+    }
+
+    return cnt;
+
+}
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        //      c0  c1  c2
+        //  r0  1   5  9
+        // r1   10 11 13
+        //  r2  12 13 15
+
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        int ans=matrix[0][0];
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(count(mid,matrix)>=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
+        }
+
+        return ans;
+
+        
+    }
+};
