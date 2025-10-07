48ee2379dca26b163a24f5f9f2946a7cced85879
Ocean <144541279+oceanja@users.noreply.github.com>
2025-10-08 00:19:59 +0530

    Time: 0 ms (100%), Space: 7.9 MB (89.04%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        // 2 7 3 6
-        // 7 2 3 6
-        // 9 9 7 3 
-        // 9 9 7 3 
-        // 2 5 3 6
-        // 
+        string s=to_string(num);
+        int n=s.length();
 
-        string number = to_string(num);
-        int n = number.length();
-        for (int i = 0; i < n; i++) {
-            int maxiIndex = -1; 
-            for (int j = i + 1; j < n; j++) {
-                
-                if (number[j] > number[i]) {
-                    if (maxiIndex == -1 || number[j] >= number[maxiIndex]) {
-                        maxiIndex = j;
-                    }
+        vector<int> maxRight(10,-1);
+
+        for(int i=0;i<n;i++){
+            int idx=s[i]-'0';
+            maxRight[idx]=i;
+        }
+
+        for(int i=0;i<n;i++){
+            char currChar=s[i];
+            int currDigit=currChar-'0';
+
+            for(int digit=9;digit>currDigit;digit--){
+
+                if(maxRight[digit]>i){
+                    swap(s[i],s[maxRight[digit]]);
+                    return stoi(s);
                 }
-            }
-            if (maxiIndex != -1) {
-                swap(number[i], number[maxiIndex]);
-                break;
+
             }
         }
 
-        int newNum = stoi(number);
-        return newNum;
+        return num;
+        
     }
 };
