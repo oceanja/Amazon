70296f86d777901136b9c78b461303efb0d8a47f
Ocean <144541279+oceanja@users.noreply.github.com>
2025-11-06 03:17:05 +0530

    Time: 0 ms (100%), Space: 18.6 MB (27.93%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,44 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-int sum=0;
+    int sum = 0;
 
-void f(TreeNode* root,string num){
+    void f(TreeNode* root, string &num) {
+        if (!root) return;
 
-    if(!root){
-        return;
+        num += to_string(root->val);
+
+        if (!root->left && !root->right) {
+            sum += stoi(num, 0, 2);
+            num.pop_back();  
+            return;
+        }
+
+        f(root->left, num);
+        f(root->right, num);
+
+        num.pop_back();  // backtrack before returning to parent
     }
 
-    num+=to_string(root->val);
-
-    if(root->left==NULL && root->right==NULL){
-        sum=sum+stoi(num,0,2);
-        return ;
-    }
-
-
-    if(root->left){
-        f(root->left,num);
-    }
-
-    if(root->right){
-        f(root->right,num);
-    }
-
-}
     int sumRootToLeaf(TreeNode* root) {
-        f(root,"");
+        string num = "";
+        f(root, num);
         return sum;
-        
     }
 };
