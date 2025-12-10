93548598f5a58aa98e584e7b2e76e5c7171cf7f6
Ocean <144541279+oceanja@users.noreply.github.com>
2025-12-10 16:07:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (5.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
+    int m,n;
+    long long MARK = (long long)INT_MIN - 1;   // safe marker outside int range
 
-void set(int row,int col,vector<vector<int>>& grid){
-    int m=grid.size();
-    int n=grid[0].size();
+    void setRowCol(int row,int col,vector<vector<long long>>& mat){
 
-    for(int i=0;i<n;i++){
-        grid[row][i]=0;
+        for(int i=0;i<n;i++){
+            mat[row][i] = (mat[row][i] != 0 ? MARK : 0);
+        }
+
+        for(int j=0;j<m;j++){
+            mat[j][col] = (mat[j][col] != 0 ? MARK : 0);
+        }
     }
 
-    for(int j=0;j<m;j++){
-        grid[j][col]=0;
-    }
-
-}
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>> grid=matrix;
+
+        m = matrix.size();
+        n = matrix[0].size();
+
+        vector<vector<long long>> mat(m, vector<long long>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = matrix[i][j];
+            }
+        }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    set(i,j,grid);
+                if(mat[i][j] == 0){
+                    setRowCol(i,j,mat);
                 }
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                matrix[i][j]=grid[i][j];
+                if(mat[i][j] == MARK){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+
+        // copy back to original matrix
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j] = (int)mat[i][j];
             }
         }
     }
